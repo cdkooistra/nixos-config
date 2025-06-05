035521d350cbfb41316a2c22fdf60e1dd96e1fbe
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2025-06-05 11:31:15 +0200

    chore: fix indents

--- modules/desktops/gnome.nix
+++ modules/desktops/gnome.nix
@@ -1,45 +1,45 @@
 { lib, config, pkgs, ... }:
 
 {
-    options = {
-        gnome.enable = lib.mkEnableOption "enable GNOME";
+  options = {
+    gnome.enable = lib.mkEnableOption "enable GNOME";
+  };
+  
+  config = lib.mkIf config.gnome.enable {
+      # Enables GDM (Wayland) and GNOME system.
+    services.xserver = {
+      enable = true;
+  
+      displayManager.gdm = {
+        enable = true;
+        wayland = true;
+      };
+  
+      desktopManager.gnome.enable = true;
     };
 
-    config = lib.mkIf config.gnome.enable {
-        # Enables GDM (Wayland) and GNOME system.
-        services.xserver = {
-            enable = true;
+    environment.systemPackages = with pkgs; [
+      gnome-tweaks
+      dconf2nix
+      gnomeExtensions.dash-to-panel
+    ];
 
-            displayManager.gdm = {
-                enable = true;
-                wayland = true;
-            };
-
-            desktopManager.gnome.enable = true;
-        };
-
-        environment.systemPackages = with pkgs; [
-            gnome-tweaks
-            dconf2nix
-            gnomeExtensions.dash-to-panel
-        ];
-
-        environment.gnome.excludePackages = with pkgs; [
-            pkgs.gnome-contacts
-            pkgs.gnome-text-editor
-            pkgs.gnome-tour
-            pkgs.gnome-maps
-            pkgs.gnome-weather
-            pkgs.gnome-clocks
-            pkgs.gnome-characters
-            pkgs.gnome-connections
-            pkgs.gnome-bluetooth # TODO: if desktop -> disable bt | else -> enable bt
-            pkgs.gnome-user-docs
-            pkgs.geary
-            pkgs.epiphany
-            pkgs.gedit
-            evince
-            yelp
-        ];
-    };
+    environment.gnome.excludePackages = with pkgs; [
+      pkgs.gnome-contacts
+      pkgs.gnome-text-editor
+      pkgs.gnome-tour
+      pkgs.gnome-maps
+      pkgs.gnome-weather
+      pkgs.gnome-clocks
+      pkgs.gnome-characters
+      pkgs.gnome-connections
+      pkgs.gnome-bluetooth # TODO: if desktop -> disable bt | else -> enable bt
+      pkgs.gnome-user-docs
+      pkgs.geary
+      pkgs.epiphany
+      pkgs.gedit
+      evince
+      yelp
+    ];
+  };
 }
