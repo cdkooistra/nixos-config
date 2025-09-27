ce59c10f182c9cb87ac6e6608ee2c529621aa494
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2025-09-27 14:31:33 +0200

    fix: nix-ld to solve vscode remote ssh

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -10,4 +10,9 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
 
+  # enable this setting to solve some issues with unpatched dynamic binaries
+  # by doing some workaround for the dynamic linker
+  # this helps set up remote vscode server :)
+  programs.nix-ld.enable = true;
+
 }
