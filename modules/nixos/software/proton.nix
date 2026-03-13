865e1d89f9db33a18d9a41e67ff313515b820449
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2026-03-13 16:51:19 +0100

    chore: add protonvpn

--- modules/nixos/software/proton.nix
+++ modules/nixos/software/proton.nix
@@ -7,17 +7,14 @@
 
 {
   options.software.proton = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-      description = "enable Proton mail + pass";
-    };
+    enable = lib.mkEnableOption "enable Proton suite";
   };
 
   config = lib.mkIf config.software.proton.enable {
-    environment.systemPackages = [
-      pkgs.proton-pass
-      pkgs.protonmail-desktop
+    environment.systemPackages = with pkgs; [
+      proton-pass
+      protonmail-desktop
+      protonvpn-gui
     ];
   };
 
