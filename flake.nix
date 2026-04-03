fee73244ab469d961764d67c86fa7a1aab24a71b
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2026-04-03 15:27:03 +0200

    chore: remove zen from flake inputs

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # zen-browser = {
-    #   url = "github:youwen5/zen-browser-flake";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
-
     agenix.url = "github:ryantm/agenix";
 
     flox = {
