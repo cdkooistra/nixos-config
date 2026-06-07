8b03f122b9f184a6f847320548d0a0e6dabaeef4
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2026-06-07 18:42:49 +0200

    chore: enable new tailscale on artemis

--- hosts/artemis/default.nix
+++ hosts/artemis/default.nix
@@ -2,13 +2,22 @@
 # Artemis, goddess of the hunt, thin and mobile workstation for productivity and development.
 #
 
-{ mkHost, network, ... }:
+{
+  mkHost,
+  network,
+  secretsDir,
+  ...
+}:
 
 mkHost {
   name = "artemis";
   arch = "x86_64-linux";
 
   system = {
+    age.identityPaths = [
+      "/home/connor/.ssh/id_ed25519"
+    ];
+
     graphics = {
       amd.enable = true;
       displaylink.enable = true;
@@ -27,7 +36,34 @@ mkHost {
       docker.enable = true;
       espanso.enable = true;
       devenv.enable = true;
-      tailscale.enable = true;
+      tailscale = {
+        enable = true;
+
+        auth = {
+          enable = true;
+          file = "${secretsDir}/tailscale.age";
+          params = {
+            preauthorized = true;
+            ephemeral = false;
+          };
+        };
+        tags = [ "tag:laptop" ];
+
+        serve = {
+          enable = false;
+          # services = {
+          #   # set up some basic server using: python3 -m http.server 8080
+          #   example-web-server = {
+          #     endpoints = {
+          #       # service endpoint with port 443 linked to local endpoint with port 8080
+          #       "tcp:443" = "http://localhost:8080";
+          #     };
+          #     advertised = true; # this is the default case, understand now, remove later
+          #   };
+          # };
+        };
+
+      };
       syncthing = {
         enable = true;
         deviceId = network.devices.artemis;
