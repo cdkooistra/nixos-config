c4ae39d7a2870b51adae4b305680d9f502e94c90
Connor Kooistra <70811244+cdkooistra@users.noreply.github.com>
2026-07-27 08:07:03 +0200

    chore: python fix and fmt

--- utils/rebuild.py
+++ utils/rebuild.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-from pathlib import Path
 import subprocess
 import sys
+from pathlib import Path
 
-def find_flake(start: Path = Path.cwd()) -> Path:
+
+def find_flake() -> Path:
+    start = Path.cwd()
     for parent in [start] + list(start.parents):
         if (parent / "public" / "flake.nix").exists():
             return parent / "public"
     raise FileNotFoundError("Could not find flake.nix")
+
 
 def main():
     if len(sys.argv) < 3:
@@ -20,9 +23,10 @@ def main():
     host = sys.argv[2]
 
     cmd = ["sudo", "nixos-rebuild", action, "--flake", f"{flake}#{host}"]
-    res = subprocess.run(cmd)
+    res = subprocess.run(cmd, check=False)
 
     sys.exit(res.returncode)
+
 
 if __name__ == "__main__":
     main()
--- utils/update.py
+++ utils/update.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
-from pathlib import Path
 import subprocess
 import sys
+from pathlib import Path
 
-def find_flake(start: Path = Path.cwd()) -> Path:
+
+def find_flake() -> Path:
+    start = Path.cwd()
     for parent in [start] + list(start.parents):
         if (parent / "public" / "flake.nix").exists():
             return parent / "public"
     raise FileNotFoundError("Could not find flake.nix")
+
 
 def main():
     if len(sys.argv) > 1:
@@ -16,12 +19,13 @@ def main():
         sys.exit(1)
 
     flake = find_flake()
-    
+
     res = subprocess.run(
-        ["sudo", "nix", "flake", "update", "--flake", f"{flake}"]
+        ["sudo", "nix", "flake", "update", "--flake", f"{flake}"], check=False
     )
 
     sys.exit(res.returncode)
+
 
 if __name__ == "__main__":
     main()
